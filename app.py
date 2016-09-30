c2457fee728d502632cc37c2227286418c863bf3
Roger González <rogergonzalez21@gmail.com>
2016-09-29 21:34:21 -0400

    Deleted useless print

--- app.py
+++ app.py
@@ -4,12 +4,13 @@ import requests, urllib
 
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 url = 'http://www.biblegateway.com/passage/?search=%s&version=RVR1960' % (urllib.parse.quote_plus(argv[1]))
-print (url)
+
 r = requests.get(url, headers=headers)
 
 soup = BeautifulSoup(r.content, 'html.parser')
 
 container = soup.find('div', {'class': 'result-text-style-normal'})
+
 for child in container:
     try:
         print (child.get_text())
